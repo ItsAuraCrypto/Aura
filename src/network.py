d2d06ced1ace0e3f0536ec9aee7499171ddd4a43
Aura Developer <poka.com@proton.me>
2024-12-01 21:03:08 +0330

    Update network.py

--- src/network.py
+++ src/network.py
@@ -1,1 +1,16 @@
+# src/network.py
+# This module manages the blockchain network and communication between nodes.
 
+class Network:
+    def __init__(self):
+        # Initialize the network with an empty list of nodes.
+        self.nodes = []
+
+    def add_node(self, node):
+        """Add a new node to the network."""
+        self.nodes.append(node)
+
+    def broadcast(self, data):
+        """Broadcast data to all nodes in the network."""
+        for node in self.nodes:
+            print(f"Broadcasting data to node {node}: {data}")
