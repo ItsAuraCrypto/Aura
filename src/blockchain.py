36b5739b8eba3d60f5be830aa3610747f62300da
Aura Developer <poka.com@proton.me>
2024-12-01 20:51:34 +0330

    Update blockchain.py

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,1 +1,13 @@
+# src/blockchain.py
+class Blockchain:
+    def __init__(self):
+        self.chain = []  
+        self.create_genesis_block() 
 
+    def create_genesis_block(self):
+        genesis_block = {"index": 0, "data": "Genesis Block"}
+        self.chain.append(genesis_block)
+
+    def add_block(self, data):
+        block = {"index": len(self.chain), "data": data}
+        self.chain.append(block)
