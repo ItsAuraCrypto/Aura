9b6d0b4e4421797cd588b2001977bc15d1ee8d47
Aura Developer <poka.com@proton.me>
2025-01-02 11:55:12 +0330

    Delete src/__init__.py
    Signed-off-by: Aura Developer <poka.com@proton.me>

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Short description of this module's purpose.
-
-Detailed explanation about what this file does and its role in the project.
-"""
-
-# src/__init__.py
-# This file marks the directory as a Python package.
